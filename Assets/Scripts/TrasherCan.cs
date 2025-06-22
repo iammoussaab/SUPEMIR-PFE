31cc121352860226560527285d8d1dd4626b6d38
iammoussaab <iammoussaab@gmail.com>
2025-06-22 02:01:26 +0100

    finish the code of the trash

--- Assets/Scripts/TrasherCan.cs
+++ Assets/Scripts/TrasherCan.cs
@@ -5,12 +5,16 @@ public class TrasherCan : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<TriggerZone>().OnEnterEvent.AddListener(insidetrash);
     }
 
+    public void insidetrash(GameObject go)
+    {
+        go.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
