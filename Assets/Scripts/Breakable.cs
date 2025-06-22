79e3d5bc56e8b647a0c5ffef276ed714bce0517f
iammoussaab <iammoussaab@gmail.com>
2025-06-22 17:55:38 +0100

    added the needed for breakable items plus the energy materiel

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -5,32 +5,36 @@ public class Breakable : MonoBehaviour
 {
 
     public List<GameObject> breakableItems; // List of breakable objects
-
+    public float timeToBreak = 2;
+    private float timer = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (var item in breakableItems)
+        foreach (var item in breakableItems)
         {
             item.SetActive(false); // Deactivate all breakable items at the start
         }
-        
+
     }
 
     public void Break()
     {
-        foreach(var item in breakableItems)
-        {
-            item.SetActive(true); // Activate all breakable items when broken
-            item.transform.parent = null; // Remove parent to allow free movement
-        }
-        Debug.Log("Breakable items activated");
-        gameObject.SetActive(false); // Optionally deactivate the original object
+        timer += Time.deltaTime;
+        if(timer > timeToBreak)
+        {    foreach(var item in breakableItems)
+            {
+                item.SetActive(true); // Activate all breakable items when broken
+                item.transform.parent = null; // Remove parent to allow free movement
+            }
+            Debug.Log("Breakable items activated");
+            gameObject.SetActive(false);
+            } // Optionally deactivate the original object
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/MeteorPisto.cs
+++ Assets/Scripts/MeteorPisto.cs
@@ -6,6 +6,15 @@ public class MeteorPisto : MonoBehaviour
 {
 
     public ParticleSystem shootingEffect; // Reference to the particle system for shooting effect
+
+    public LayerMask layerMask; // Layer mask to filter raycast hits
+    public Transform shootsource; // Transform from where the shooting effect originates
+    public float shootRange = 10; // Range of the shooting effect
+
+    private bool rayActive = false; // Flag to check if raycasting is active
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,12 +27,14 @@ public class MeteorPisto : MonoBehaviour
     {
         shootingEffect.Play(); // Play the shooting effect particle system
         // Logic to start shooting
+        rayActive = true; // Set rayActive to true to indicate shooting is active
         Debug.Log("Shooting started");
     }
 
     public void stopShooting()
     {
         shootingEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear); // Correct enum value
+        rayActive = false; // Set rayActive to false to indicate shooting is stopped
         // Logic to stop shooting
         Debug.Log("Shooting stopped");
     }
@@ -31,6 +42,18 @@ public class MeteorPisto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rayActive)
+            RaycastCheck(); // Check for raycast hits every frame
+    }
+
+    void RaycastCheck()
+    {
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(shootsource.position, shootsource.forward, out hit, shootRange, layerMask);
+        if(hasHit)
+        {
+            hit.transform.gameObject.SendMessage("Break", SendMessageOptions.DontRequireReceiver); // Send a message to the hit object to break it
+        }
     }
 }
