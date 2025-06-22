b9cc4bcd1b4b0f9f952d5c8704d4d91a36f58e50
iammoussaab <iammoussaab@gmail.com>
2025-06-22 16:08:05 +0100

    adding the particles system and added the breakable and more interactions

--- Assets/Scripts/MeteorPisto.cs
+++ Assets/Scripts/MeteorPisto.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using UnityEngine.XR;
+
 
 public class MeteorPisto : MonoBehaviour
 {
+
+    public ParticleSystem shootingEffect; // Reference to the particle system for shooting effect
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        grabInteractable.activated.AddListener(x => startShooting());
+        grabInteractable.deactivated.AddListener(x => stopShooting());
+    }
+
+    public void startShooting()
+    {
+        shootingEffect.Play(); // Play the shooting effect particle system
+        // Logic to start shooting
+        Debug.Log("Shooting started");
+    }
+
+    public void stopShooting()
+    {
+        shootingEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear); // Correct enum value
+        // Logic to stop shooting
+        Debug.Log("Shooting stopped");
     }
 
     // Update is called once per frame
